86ea7887e862ebfdf0f5716292d6fb1ee9a45c64
DarkProd-off <yaya.dz66@gmail.com>
2022-01-19 17:52:29 +0100

    add main.py structure

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 #Main.py
 #Main Program othello or awele
+
+import sys
+sys.path.append("..")
+
+import awele
+sys.path.append("../..")
+
+import game
+game.game = awele
+sys.path.append("../Joueurs")
+
+import joueur_humain
+import joueur_random
+
+game.joueur1 = joueur_humain 
+game.joueur2 = joueur_random 
