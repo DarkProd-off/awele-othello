7ca2f96aadccdb673f3e649f9a0af7ba6458bd3d
Chock2b <chadi.tawbi2002@gmail.com>
2022-02-20 14:29:58 +0100

    Add AI to Othello

--- Othello/Joueurs/joueur_aleatoire.py
+++ Othello/Joueurs/joueur_aleatoire.py
@@ -6,16 +6,62 @@ import game
 
 import random
 
+tmp=0
 
 def saisieCoup(jeu):
+	global tmp
 	""" jeu -> coup
 		Retourne un coup a jouer
 	"""
 	coupsValides = game.getCoupsValides(jeu)
 	#print("Liste des coups jouables: "+str(coupsValides))
-	if len(coupsValides) > 0:
-		selectedCoup = random.choice(coupsValides)
+	#if len(coupsValides) > 0:
+	if tmp<=4:
+		tmp+=1
+		return random.choice(coupsValides)
 	else:
-		selectedCoup = []
+		return decision(jeu,coupsValides)
 
-	return selectedCoup
+
+
+def decision(jeu, lstCoupsValides):
+	bestScore = -1000
+	bestCoup = []
+
+	for i in range(len(lstCoupsValides)):
+		currCoup = lstCoupsValides[i]
+
+		scoreGained = estimation(jeu, currCoup)
+
+		if scoreGained >= bestScore:
+			bestCoup = currCoup
+
+		print(scoreGained)
+		print(bestScore)
+
+
+	return bestCoup
+
+
+def estimation(jeu, coup):
+	jeuCpy = game.getCopieJeu(jeu)
+
+	currEval = evaluation(jeuCpy, coup)
+
+
+	game.joueCoup(jeuCpy, coup)
+
+
+	finalEval = evaluation(jeuCpy, coup)
+
+	return currEval + finalEval
+
+
+def scoreDiff(jeu):
+	scores = game.getScores(jeu)
+	return scores[1] - scores[0]
+
+def evaluation(jeu, coup):
+	diffScores = scoreDiff(jeu)
+
+	return diffScores
